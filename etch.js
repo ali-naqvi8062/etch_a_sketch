eae6182ac5a9e93bdf70d5185284e7c442adf7a4
Ali Naqvi <ali.naqvi8062@gmail.com>
2023-07-02 22:43:14 -0400

    set limiter to grid

--- etch.js
+++ etch.js
@@ -1,22 +1,36 @@
 const main = document.querySelector('.options');
 const btn = document.querySelector('.trigger');
+const boxes = document.createElement('div');
+const nmb = document.querySelectorAll('.box').length;
+
+
 
 btn.addEventListener('click', function (e) {
+
+    const nmb = document.querySelectorAll('.box').length;
+    console.log(nmb);
+
+    if (nmb == 256){
+        return;
+    }
 
     for (let i=0; i < 16; i++){
 
         const boxes = document.createElement('div');
         main.appendChild(boxes);
         boxes.classList.add('row');
-        
+            
         for (let k=0; k < 16; k++){
-
+    
             const miniboxes = document.createElement('div');
             boxes.appendChild(miniboxes);
             miniboxes.classList.add('box')
 
         }
-
     }
+
+    
+
+
 
 });
