d76b56ddec7aa4328b257e8b8d6da3b600d0585e
Ali Naqvi <ali.naqvi8062@gmail.com>
2023-07-02 21:48:11 -0400

    Add for loop that creates grid for sketchpad

--- etch.js
+++ etch.js
@@ -1,1 +1,22 @@
-const main = document.querySelector('.title');
+const main = document.querySelector('.options');
+const btn = document.querySelector('.trigger');
+
+btn.addEventListener('click', function (e) {
+
+    for (let i=0; i < 16; i++){
+
+        const boxes = document.createElement('div');
+        main.appendChild(boxes);
+        boxes.classList.add('row');
+        
+        for (let k=0; k < 16; k++){
+
+            const miniboxes = document.createElement('div');
+            boxes.appendChild(miniboxes);
+            miniboxes.classList.add('box')
+
+        }
+
+    }
+
+});
